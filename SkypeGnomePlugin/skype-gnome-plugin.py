42fef9dc1eca6822042a13c8fc0ad48fb02c5b75
Farkas Zoltán <f.zoli@mailbox.hu>
2014-09-25 01:12:26 +0200

    skype gnome plugin error handling

--- SkypeGnomePlugin/skype-gnome-plugin.py
+++ SkypeGnomePlugin/skype-gnome-plugin.py
@@ -5,14 +5,16 @@ import Skype4Py
 import dbus
 import dbus.glib
 import gobject
+from threading import Thread
 
 class SkypeGnomePlugin:
 
   def __init__(self):
+    self.__init_global_error_handler()
     self.__init_dbus()
     self.__init_skype()
     self.__start_main_loop()
-    self.restoreStatus = False
+#    self.restoreStatus = False
   
   def __dbus_screensaver_handler(self, p):
     if p == 1:
@@ -27,25 +29,33 @@ class SkypeGnomePlugin:
       if status != Skype4Py.apiAttachRefused:
         self.__on_skype_closed()
   
+  def __set_skype_status(self, status):
+    Thread(target = self.skype.ChangeUserStatus, args = (status, )).start()
+    print 'Set to ' + status + '.'
+
   def __on_screen_locked(self):
-    print 'Screen locked.'
+    print 'Screen has locked.'
     if self.skype.CurrentUserStatus == 'ONLINE':
-      self.restoreStatus = True
-      self.skype.ChangeUserStatus('AWAY')
+#      self.restoreStatus = True
+      self.__set_skype_status('AWAY')
   
   def __on_screen_unlocked(self):
-    print 'Screen unlocked.'
-    if self.restoreStatus:
-      self.restoreStatus = False
-      self.skype.ChangeUserStatus('ONLINE')
+    print 'Screen has been unlocked.'
+#    if self.restoreStatus:
+#      self.restoreStatus = False
+    if self.skype.CurrentUserStatus == 'AWAY':
+      self.__set_skype_status('ONLINE')
   
   def __on_skype_attached(self):
-    print 'Skype attached.'
+    print 'Skype plugin has attached.'
   
   def __on_skype_closed(self):
-    print 'Skype closed.'
+    print 'Skype has been closed.'
     self.__stop_main_loop()
   
+  def __init_global_error_handler(self):
+    sys.excepthook = self.__on_error
+
   def __init_dbus(self):
     self.bus = dbus.SessionBus()
     self.bus.add_signal_receiver(self.__dbus_screensaver_handler, dbus_interface='org.gnome.ScreenSaver', signal_name='ActiveChanged')
@@ -54,11 +64,21 @@ class SkypeGnomePlugin:
     self.skype = Skype4Py.Skype()
     if not self.skype.Client.IsRunning:
       print >>sys.stderr, 'Skype is not running.'
-      sys.exit(-1)
+      sys.exit(1)
     self.skype.FriendlyName = 'GnomePlugin'
     self.skype.OnAttachmentStatus = self.__skype_attachment_handler;
-    self.skype.Attach()
+    try:
+      self.skype.Attach()
+    except Skype4Py.errors.SkypeAPIError:
+      print >>sys.stderr, 'Skype plugin access denyed.'
+      sys.exit(1)
   
+  def __on_error(self, exctype, value, traceback):
+    print 'Unexpected error.'
+    self.__stop_main_loop()
+    sys.__excepthook__(exctype, value, traceback)
+    sys.exit(1)
+
   def __start_main_loop(self):
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     self.loop = gobject.MainLoop()
@@ -67,7 +87,7 @@ class SkypeGnomePlugin:
   def __stop_main_loop(self):
     print 'Bye'
     self.loop.quit()
-
+  
 if __name__ == '__main__':
   SkypeGnomePlugin()
   
